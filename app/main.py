0c739cff062677a9c2e61eb2fcc7ab86bb39ead6
Hunach <sasha2001sek@gmail.com>
2026-02-09 17:27:25 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from hashlib import sha256
 import multiprocessing as mp
 
 
-PASSWORDS_TO_BRUTE_FORCE = [
+PASSWORDS_TO_BRUTE_FORCE = {
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
     "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
     "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
@@ -14,7 +14,7 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
     "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
-]
+}
 
 
 def sha256_hash_str(to_hash: str) -> str:
@@ -27,10 +27,11 @@ def brute_range_print(process_idx: int, start: int, end: int) -> None:
     for i in range(start, end):
         # Форматуємо число в 8-значний рядок (00000000)
         candidate = f"{i:08d}"
-        h = sha256_hash_str(candidate)
+        unh = sha256_hash_str(candidate)
 
-        if h in PASSWORDS_TO_BRUTE_FORCE:
-            print(f"\n[!] Process {process_idx} found password: {candidate} for hash {h}")
+        if unh in PASSWORDS_TO_BRUTE_FORCE:
+            print(f"\n[!] Process {process_idx} found password: "
+                  f"{candidate} for hash {unh}")
     print(f"Process {process_idx} finished.")
 
 
